f3ac2ff1f555ae71c9cbd07186edb0b7029c4e6e
Ahamed R <ahamedrasheedhot@gmail.com>
2021-05-15 03:37:26 +0530

    feat: PWA Initialized

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
+  useEffect(() => {
+    /**
+     * Enable and poit to Service worker file
+     */
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+      })
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
